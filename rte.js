ca2c66b09d4c7c69684979effad65cc3432f6844
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-10-25 18:11:53 +0300

    SDFGH

--- rte.js
+++ rte.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let a = [1, 2, 3, 4];
 for(let k in a){
     console.log(a[k])
@@ -9,9 +8,6 @@ console.log("");
 let c = ["q", "w", "e", "r", "t", "y"];
 for (let d in c){
     console.log(c[d])
-=======
-let s = {
-    name: "Vika",
-    age: 20
->>>>>>> 464ad01df39c9ea505c85e50598db2a80b9be97b
 };
+
+console.log("")
